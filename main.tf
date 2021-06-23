d72826d63d01ae8a159589adf3495059590601ef
Dexarc <76424945+Dexarc@users.noreply.github.com>
2021-06-23 10:10:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,14 +12,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-077e31c4939f6a2f3"
+  ami           = "ami-0835374e611a23aa7"
   instance_type = "t2.micro"
   key_name = "ec2"
-  user_data = <<EOF
-  #cloud-config
-  runcmd:
-    - sudo yum update -y
-EOF
   provisioner "local-exec" {
     command = "echo ${self.public_ip} > hosts.inv"
   }
