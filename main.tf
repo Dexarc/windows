b03d0319d5032196340ac1f068f3278d30bc1ec6
Dexarc <76424945+Dexarc@users.noreply.github.com>
2021-06-23 09:39:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0835374e611a23aa7"
+  instance_type = "t2.micro"
+  key_name = "ec2"  
