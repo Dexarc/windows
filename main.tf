ebfc628f8e13472d2ee14fcfa8ef41064750ab13
Dexarc <76424945+Dexarc@users.noreply.github.com>
2021-06-24 16:21:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,20 +15,8 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   key_name = "ec2"
   security_groups = ["launch-wizard-2"]
-  get_password_data = "true"
-  connection {
-    type = "winrm"
-    user = "Administrator"
-    port = 5986
-    password = "${rsadecrypt(self.password_data, file("ec.pem"))}"
-    host = "${self.public_ip}"
-  }
+  user_data = "${file("userdata.txt")}"
   provisioner "local-exec" {
     command = "echo ${self.public_ip} > hosts.txt"
-  }
-  provisioner "remote-exec" {
-    inline = [
-         "powershell -ExecutionPolicy Unrestricted -File winrm.ps1"
-        ]
   }
 }
