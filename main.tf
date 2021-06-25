5580c3f833e370d24485fe1b78b31c04f41277fe
Dexarc <76424945+Dexarc@users.noreply.github.com>
2021-06-25 10:54:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,8 +15,27 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   key_name = "ec2"
   security_groups = ["launch-wizard-2"]
+  get_password_data = "true"
   user_data = "${file("userdata.txt")}"
   provisioner "local-exec" {
-    command = "echo ${self.public_ip} > hosts.txt"
+    command = "echo [win] > hosts.txt"
+  }
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} >> hosts.txt"
+  }
+  provisioner "local-exec" {
+    command =  "echo [win:vars] >> hosts.txt"
+  }
+    provisioner "local-exec" {
+    command =  "echo 'ansible_user = \"Administrator\"' >> hosts.txt"
+  }
+    provisioner "local-exec" {
+    command =  "echo ansible_password = '${rsadecrypt(self.password_data, file("C:\\terraform\\Modules\\vnet1\\ec.pem"))}' >> hosts.txt"
+  }
+    provisioner "local-exec" {
+    command =  "echo 'ansible_connection = \"winrm\"' >> hosts.txt"
+  }
+    provisioner "local-exec" {
+    command =  "echo ansible_winrm_server_cert_validation = ignore >> hosts.txt"
   }
 }
